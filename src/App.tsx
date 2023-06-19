611b9a40e1b88aeda5a6807b7c330777e25b03c0
Vladimir Kolov <kolov.vladimir.1998@gmail.com>
2023-06-19 10:35:36 +0700

    Init commit, project cleaned and setup complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Button from "./components/Button/Button"
 
 function App() {
+
+    const handleClick = () => {
+        console.log('hello')
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <Button handleClick={handleClick} >
+              Hello
+          </Button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
